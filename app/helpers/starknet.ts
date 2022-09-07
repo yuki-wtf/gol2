b4d812ec54708f5ac3ee4e7831fa44826657d075
Tsotne Nazarashvili <cotne.95@gmail.com>
2022-09-07 19:23:39 +0400

    update wallet address formatting

--- app/helpers/starknet.ts
+++ app/helpers/starknet.ts
@@ -2,13 +2,11 @@ import { getChecksumAddress } from 'starknet'
 import type { BigNumberish } from 'starknet/dist/utils/number'
 
 export function getShortChecksumAddress(address: BigNumberish | string): string {
-  const checksumAddress = getChecksumAddress(address)
+  const checksumAddress = getChecksumAddress(address).toLowerCase()
 
   return (
-    checksumAddress.slice(0, 2) +
-    ' ' +
-    checksumAddress.slice(2, 6) +
-    ' ... ' +
+    checksumAddress.slice(0, 6) +
+    '...' +
     checksumAddress.slice(checksumAddress.length - 4)
   )
 }
@@ -18,17 +16,17 @@ if (import.meta.vitest) {
 
   it('getShortChecksumAddress #1', () => {
     const long = '0x2bf0d1951393886de60fe3ef3d16482a1998d33e2572fac8b1fcfdcd59ead60'
-    const short = '0x 02bF ... Ad60'
+    const short = '0x02bf...ad60'
     expect(getShortChecksumAddress(long)).toBe(short)
   })
   it('getShortChecksumAddress #2', () => {
     const long = '1242183891794214040002074274514933996473193662983245409254398531555775393120'
-    const short = '0x 02bF ... Ad60'
+    const short = '0x02bf...ad60'
     expect(getShortChecksumAddress(long)).toBe(short)
   })
   it('getShortChecksumAddress #2', () => {
     const long = '932097343094516780977311641685690157093908922685193531952538698512374685643'
-    const short = '0x 020F ... dFcB'
+    const short = '0x020f...dfcb'
     expect(getShortChecksumAddress(long)).toBe(short)
   })
 }
