231641fd292593720ac79f29e23b38354168c511
Tsotne Nazarashvili <cotne.95@gmail.com>
2022-09-16 11:19:04 +0400

    ask wallet to change network

--- app/hooks/useUser.tsx
+++ app/hooks/useUser.tsx
@@ -1,6 +1,9 @@
 import { useFetcher, useLoaderData } from '@remix-run/react'
 import { useStarknet } from '@starknet-react/core'
 import { createContext, useContext, useState } from 'react'
+import type { AccountInterface } from 'starknet3'
+import { getChecksumAddress } from 'starknet3'
+import { StarknetChainId } from 'starknet3/dist/constants'
 import type { loader } from '~/root'
 import { useLayoutEffectX } from './useLayoutEffectX'
 
@@ -19,13 +22,43 @@ export function UserProvider({ children }: Props) {
   const starknet = useStarknet()
   const data = useLoaderData<typeof loader>()
 
-  const { account } = starknet
+  const { account, connectors } = starknet
   console.log('account UserIdProvider', account)
 
   const user = data.userId != null && data.balance != null ? { userId: data.userId, balance: data.balance } : null
 
   const [userId, setUserId] = useState(data.userId)
   const fetcher = useFetcher()
+
+  const [accountObj, setAccountObj] = useState<AccountInterface>()
+
+  useLayoutEffectX(() => {
+    ;(async () => {
+      for (const connector of connectors) {
+        const accountObj = await connector.account()
+        if (getChecksumAddress(accountObj.address) === getChecksumAddress(account)) {
+          setAccountObj(accountObj)
+        }
+      }
+    })()
+  }, [account, connectors])
+
+  useLayoutEffectX(() => {
+    if (accountObj == null) return
+
+    const currentNet = location.hostname === 'gol2.io' ? StarknetChainId.MAINNET : StarknetChainId.TESTNET
+
+    if (currentNet !== accountObj.chainId) {
+      if (window.starknet != null) {
+        window.starknet.request({
+          type: 'wallet_switchStarknetChain',
+          params: {
+            chainId: currentNet,
+          },
+        })
+      }
+    }
+  }, [accountObj])
 
   useLayoutEffectX(() => {
     console.log([userId, account])
