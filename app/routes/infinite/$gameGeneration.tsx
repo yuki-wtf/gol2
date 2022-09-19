177a66f91e2fdfe6c7efd8f4d09af87b655fe31e
Tsotne Nazarashvili <cotne.95@gmail.com>
2022-09-19 16:17:10 +0400

    add twitter:title

--- app/routes/infinite/$gameGeneration.tsx
+++ app/routes/infinite/$gameGeneration.tsx
@@ -8,6 +8,7 @@ export const meta: MetaFunction = ({ params, parentsData }) => {
     description:
       'GoL2 a fun and interactive way to introduce new users and developers to smart contracts written in Cairo. With Starknet. Game of Life...',
     'twitter:card': 'summary_large_image',
+    'twitter:title': 'CREATE GAMES. GIVE LIFE. EVOLVE.',
     'og:image': `${parentsData.root.env.BASE_URL}/api/images/infinite/${params.gameGeneration}.png`,
     'og:image:type': 'image/png',
     'og:image:width': String(514 * 4),
